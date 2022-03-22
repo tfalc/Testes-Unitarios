14d61316a0b4ce4ce22fc00ee9a886e2762f96f7
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-03-21 23:53:20 -0300

    Initial commit

---
+++ src/main/java/com/tfalcao/unitarytests/UnitarytestsApplication.java
@@ -0,0 +1,13 @@
+package com.tfalcao.unitarytests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitarytestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitarytestsApplication.class, args);
+	}
+
+}
